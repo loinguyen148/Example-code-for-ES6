181b4ee94f6af798717e088dcd6b1ce6846979a3
raynguyen <raynguyen148@gmail.com>
2018-09-27 23:15:55 +0700

    Example for: Let, const, var scope, classes, modules

--- scripts.js
+++ scripts.js
@@ -203,3 +203,213 @@ asyncFunc2 = async() => {
   console.warn(await b);
   console.warn(await c);
 }
+
+/* Block Scope - Let and Const */
+const fn1 = () => {
+  var myVar = 1;
+  
+  console.log(myVar); // 1
+  console.log(otherVar); //undefined
+
+  otherVar = 5;
+
+  if (myVar > 0) {
+    var myVar = 2;
+    var childVar = true;
+
+    console.log(myVar); // 2
+  }
+
+  var otherVar; 
+
+  console.log(otherVar); // 5
+  console.log(myVar === 1); // false
+  console.log(childVar); //true
+};
+
+const fn2 = () => {
+  const constValue = 10;
+
+  if (true) {
+    var varValue = constValue;
+    let letValue = constValue;
+
+    console.log(varValue, letValue); // 10, 10
+  }
+
+  console.log(varValue); // 10
+
+  try {
+    console.log(letValue); // Can not access
+    
+  } catch (e) {
+    console.log(e); // Error for using let vararible
+  }
+};
+
+const fn3 = () => {
+  const value = 10;
+
+  if (value) {
+		let letValue = value;
+    let letValue = 'foo'; // error
+    // Same results when use 'const'
+	}
+};
+
+const fn4 = () => {
+  let myLet = 10;
+  const myConst = 30;
+
+  {
+    let myLet = 20;
+    const myConst = 40;
+
+    console.log(myLet); // 20
+    console.log(myConst); // 40
+  }
+
+  console.log(myLet); // 10
+  console.log(myConst); // 30
+};
+
+const myConst; // Err
+
+const constValue = 10;
+constValue = 20; // Err
+
+const fn5 = () => {
+  const constObj = { name: 'Default', age: 10 };
+  constObj.name = 'Kenvin';
+  console.log(constObj)
+  constObj = 'Sample string'; // Err
+}
+
+const fn6 = () => {
+  const otherConstObj = Object.freeze({ name: 'Default', age: 10, history: [1,2,3] });
+  otherConstObj.name = 'Kenvin';
+  otherConstObj.history.push(4);
+  console.log(otherConstObj);
+  otherConstObj.history = [10, 9, 8];
+  console.log(otherConstObj);
+}
+
+const fn7 = () => {
+  const myFn = function() { // temporal dead zone
+		console.log(value);
+  }
+  
+  let value = 'foo';
+  myFn();
+}
+
+const fn8 = () => {
+  for (var i = 0; i < 5; i++) {
+		// ..
+  }
+  
+	for (let j = 0; j < 5; j++) {
+		// ..
+	}
+
+	console.log(i);
+	console.log(j);
+}
+
+const fn9 = (value) => {
+  let value = 10; // err
+  {
+    console.log(value);
+  }
+};
+
+/*
+  Use var at the top fn
+  Use const for variables you want to be immutable (primitive: Number, String, Boolean, etc).
+  Use Object.freeze in concert to make the object truly immutable. 
+  'Var' can use everywhere in current fn scope
+*/
+
+/* Class */
+const newClass = new MyClass('Kenvin'); // Error
+
+class MyClass {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const newClass1 = new MyClass('Kenvin');
+
+// Class Expression
+let MySchool = class { // Unnamed
+  constructor(name) {
+    this.name = name;
+  }
+};
+
+console.log(MySchool.name);
+
+let myCar = class Car { // Unnamed
+  constructor(name) {
+    this.name = name;
+  }
+};
+
+const car1 = new myCar('Car 1');
+
+class ExampleClass {
+  constructor(name) {
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  setName(name) {
+    this.name = name
+  }
+
+  static getDefaultName() {
+    return 'Kenvin';
+  }
+}
+
+class ParentClass {
+  constructor(name) {
+    this.name = name;
+  }
+
+  action() {
+    console.log(this.name);
+  }
+}
+
+class ChildClass extends ParentClass {
+  constructor(name) {
+    super(name);
+  }
+
+  action() {
+    super.action();
+  }
+}
+
+const childClass = new ChildClass('Baby');
+childClass.action();
+console.log(childClass instanceof ChildClass)
+console.log(childClass instanceof ParentClass)
+console.log(ChildClass instanceof ParentClass)
+
+/* Modules */
+export const numberOne = 1;
+export const sum = ({x = 0, y = 0}) => { return x + y; }
+
+const minus = ({x = 0, y = 0}) => { return x - y; }
+const multiple = ({x = 0, y = 0}) => { return x * y; }
+const math = () => {  }; // private fn
+
+export { minus, multiple };
+
+import { numberOne, sum, minus, multiple as multip } from 'xx.js';
